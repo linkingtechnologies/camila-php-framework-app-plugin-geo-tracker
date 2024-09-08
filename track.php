5066c96f76fb30868b31565f87d6c8d0f2460e01
umbi <umbi@linkingtechnologies.it>
2024-09-08 10:53:17 +0200

    Update track.php

--- track.php
+++ track.php
@@ -1,23 +1,73 @@
 <?php
-$logDir = __DIR__ . '../../../var/log';
-if (!file_exists($logDir)) {
-    mkdir($logDir, 0777, true);
+require('../../var/config.php');
+require('../../../../lib/adodb5/adodb.inc.php');
+
+$db = NewADOConnection(CAMILA_DB_DSN);
+
+$json = file_get_contents('php://input');
+if (!empty($json)) {
+	$d = json_decode($json);
+
+	if (json_last_error() === JSON_ERROR_NONE) {
+		$rid = uniqid();
+		$data = [
+		'id' => $d->tid.$rid.$d->_id.$d->tst,
+		'tst' => date('Y-m-d H:i:s', $d->tst),
+		'inserted_at' => date('Y-m-d H:i:s', time())
+		];
+		addProperty($d, '_id', $data);
+		addProperty($d, '_type', $data);
+		addProperty($d, 'created_at', $data);
+		addProperty($d, 'lat', $data);
+		addProperty($d, 'lon', $data);
+		addProperty($d, 'topic', $data);
+		addProperty($d, 'tid', $data);
+		addProperty($d, 'BSSID', $data);
+		addProperty($d, 'SSID', $data);
+		addProperty($d, 'acc', $data);
+		addProperty($d, 'alt', $data);
+		addProperty($d, 'batt', $data);
+		addProperty($d, 'bs', $data);
+		addProperty($d, 'conn', $data);
+		addProperty($d, 'm', $data);
+		addProperty($d, 't', $data);
+		addProperty($d, 'vac', $data);
+		addProperty($d, 'vel', $data);
+		addProperty($d, 'cog', $data);
+		addProperty($d, 'rad', $data);
+		addProperty($d, 'p', $data);
+		addProperty($d, 'poi', $data);
+		addProperty($d, 'tag', $data);
+		addProperty($d, 'inregions', $data);
+		addProperty($d, 'inrids', $data);
+		
+		$table = CAMILA_APPLICATION_PREFIX.'geotracking';
+		$result = $db->AutoExecute($table, $data, 'INSERT');
+
+		if ($result) {
+			echo "OK";
+		} else {
+			header("HTTP/1.1 500 Internal Server Error");
+			echo "KO: " . $db->ErrorMsg();
+		}
+	} else {
+		header("HTTP/1.1 500 Internal Server Error");
+		echo "KO: " . json_last_error_msg();
+	}
 }
-$logFile = $logDir . '/geotracker.txt';
-$getParams = $_GET;
-$postParams = $_POST;
-$rawPayload = file_get_contents('php://input');
-$data = [];
-if (!empty($getParams)) {
-    $data['GET'] = $getParams;
+
+function addProperty($object, $property, &$data) {
+    if (is_object($object) && property_exists($object, $property)) {
+		if ($property == '_type') {
+			$data['type'] = $object->$property;
+		} if ($property == '_id') {
+			$data['aid'] = $object->$property;
+		} if ($property == 'created_at') {
+			$data[$property] = date('Y-m-d H:i:s', $object->$property);
+		} else {
+			$data[$property] = $object->$property;
+		}
+    }
 }
-if (!empty($postParams)) {
-    $data['POST'] = $postParams;
-}
-if (!empty($rawPayload)) {
-    $data['RAW_PAYLOAD'] = $rawPayload;
-}
-$logEntry = date('Y-m-d H:i:s') . " - " . print_r($data, true) . PHP_EOL;
-file_put_contents($logFile, $logEntry, FILE_APPEND);
-echo "Dati e payload salvati nel log.";
+
 ?>
