fcabb336fbd1750627ca3880937412fb3f05eafe
umbi <umbi@linkingtechnologies.it>
2024-09-20 20:59:04 +0200

    Update dashboard_map.inc.php

--- dashboard_map.inc.php
+++ dashboard_map.inc.php
@@ -24,13 +24,15 @@ function drawMap() {
 
 	if ($mapName != '')
 	{
-		$stmt1 = 'SELECT id as tracker, ${'.$serviceTable.'.NOME} as risorsa, ${'.$serviceTable.'.LETTERA} as lettera, ${'.$serviceTable.'.COLORE} as colore, ${'.$serviceTable.'.LATITUDINE} AS latitudine,${'.$serviceTable.'.LONGITUDINE} AS longitudine, NULL as tst FROM ${'.$serviceTable.'} WHERE ${'.$serviceTable.'.LATITUDINE} <> "" ORDER BY ${'.$serviceTable.'.ORDINE}';
-		$stmt2 = 'SELECT DISTINCT g.tracker, ${tracker.risorsa} as risorsa,${tracker.lettera} as lettera,${tracker.colore} as colore, g.lat AS latitudine, g.lon AS longitudine, g.tst FROM geotracker_geotracking g JOIN ( SELECT tracker, MAX(tst) AS max_tst FROM geotracker_geotracking GROUP BY tracker ) latest ON g.tracker = latest.tracker AND g.tst = latest.max_tst INNER JOIN ${TRACKER} ON ${tracker.tracker}=g.tracker ORDER BY ${tracker.risorsa}';
-		$queryList = $camilaWT->parseWorktableSqlStatement('('.$stmt1.') UNION ('.$stmt2. ')');
+		$stmt1 = 'SELECT id as tracker, ${'.$serviceTable.'.NOME} as risorsa, ${'.$serviceTable.'.LETTERA} as lettera, ${'.$serviceTable.'.COLORE} as colore, ${'.$serviceTable.'.LATITUDINE} AS latitudine,${'.$serviceTable.'.LONGITUDINE} AS longitudine, NULL as tst,${'.$serviceTable.'.ORDINE} as ORDINE FROM ${'.$serviceTable.'} WHERE ${'.$serviceTable.'.LATITUDINE} <> ""';
+		$stmt2 = 'SELECT DISTINCT g.tracker, ${tracker.risorsa} as risorsa,${tracker.lettera} as lettera,${tracker.colore} as colore, g.lat AS latitudine, g.lon AS longitudine, g.tst, ${tracker.ordine} AS ORDINE FROM geotracker_geotracking g JOIN ( SELECT tracker, MAX(tst) AS max_tst FROM geotracker_geotracking GROUP BY tracker ) latest ON g.tracker = latest.tracker AND g.tst = latest.max_tst INNER JOIN ${TRACKER} ON ${tracker.tracker}=g.tracker';
+		$queryList = $stmt1.' UNION '.$stmt2;
+		$stmt1.= ' ORDER BY ${'.$serviceTable.'.ORDINE},${'.$serviceTable.'.NOME}';
+		$stmt2.= ' ORDER BY ${tracker.ordine},${tracker.risorsa}';
 
 		$TBS = new clsTinyButStrong();
 		$TBS->SetOption(array('render'=>TBS_OUTPUT));
-		$TBS->SetOption('noerr', false);
+		$TBS->SetOption('noerr', true);
 		$TBS->SetVarRefItem('apikey', $params['chiave_mappa_google']);
 
 		$TBS->LoadTemplate(CAMILA_APP_PATH.'/plugins/'.basename(dirname(__FILE__)).'/templates/tbs/it/gmap.htm');
